b65aafa3cb96c67a29ecc0c472dbf85e7ead6b80
djuleayo <florex.satis@gmail.com>
2019-03-22 00:21:37 +0100

    add watch test. format code

--- src/cron/diminishedCron.ts
+++ src/cron/diminishedCron.ts
@@ -6,14 +6,14 @@ const peg = require('pegjs')
 
 type Range = ["range", number, number]
 type PlaceElement = ["interval", number, number] | Range | "*"
-type PatternObject =  {
-    ms: Array<PlaceElement>, 
-    s: Array<PlaceElement>,
-    min: Array<PlaceElement>,
-    h: Array<PlaceElement>,
-    dm: Array<PlaceElement>,
-    m: Array<PlaceElement>,
-    dw: Array<PlaceElement>,
+type PatternObject = {
+  ms: Array<PlaceElement>,
+  s: Array<PlaceElement>,
+  min: Array<PlaceElement>,
+  h: Array<PlaceElement>,
+  dm: Array<PlaceElement>,
+  m: Array<PlaceElement>,
+  dw: Array<PlaceElement>,
 }
 const TimeUnit = ["ms", "s", "min", "h", "dm", "m", "dw"]
 const timeUnitValue = DateUtil.timeUnitValue;
@@ -24,106 +24,107 @@ const timeUnitValue = DateUtil.timeUnitValue;
  * use [Date, Date] instead. DiminishedCron is same as cron but may have fewer
  * places then cron does.
  */
-export class DiminishedCron
-{
-    static parser = peg.generate(cronGrammar)
+export class DiminishedCron {
+  static parser = peg.generate(cronGrammar)
 
-    patternObject: PatternObject
+  patternObject: PatternObject
 
-    constructor(pattern: string) {
-        try {
-            this.patternObject = DiminishedCron.parser.parse(pattern)
-            this.purgeStars()
-            this.checkPatternSemantics()
-            this.purgeRanges()
-        } catch (error) {
-            throw error
-        }
+  constructor(pattern: string) {
+    try {
+      this.patternObject = DiminishedCron.parser.parse(pattern)
+      this.purgeStars()
+      this.checkPatternSemantics()
+      this.purgeRanges()
+    } catch (error) {
+      throw error
     }
+  }
 
-    getTimeSet(i: Interval): Array<Interval | Date> {
-        const retVal = []
+  getTimeSet(i: Interval): Array<Interval | Date> {
+    const retVal = []
 
-        
 
-        return retVal
-    }
 
-/* PRIVATE ⛔️-----------------------------------------------------------------*/
-    /**
-     * Make places made of * undefined
-     */
-    private purgeStars() {
-        TimeUnit.forEach(unit => {
-            const unitVal = this.patternObject[unit]
-            
-            if (!unitVal || unitVal.length === 1 && unitVal[0] === "*")
-               this.patternObject[unit] = void 0
-        })
-    }
-    private purgeRanges() {
-        this.iteratePlaces((place, unit) => {
-            place.map(placeElement => {
-                if (!this.isRange(placeElement)) return
-                
-            })
-        })
-    }
+    return retVal
+  }
 
-    private isRange(a: PlaceElement): boolean {
-        return a[0] === 'range'
-    }
-    private spreadRange(r: Range): Array<number> {
-        const retVal = []
-        for(let i = r[1]; i <= r[2]; i++)
-            retVal.push(i)
-        return retVal
-    }
+  /* PRIVATE ⛔️-----------------------------------------------------------------*/
+  /**
+   * Make places made of * undefined
+   */
+  private purgeStars() {
+    TimeUnit.forEach(unit => {
+      const unitVal = this.patternObject[unit]
 
-    private iteratePlaces(cb: (place: Array<PlaceElement>, unit: TimeUnit) => void) {
-        for(let unit of TimeUnit) {
-            let value = this.patternObject[unit];
-            if (!value) continue
-            cb(value, <TimeUnit>unit);
-        }
-    }
-    /**
-     * Check if patternObject satisfies TimeUnitValue 
-     */
-    private checkPatternSemantics(): void {
-        this.iteratePlaces(this.checkPlaceSemantics)
-    }
+      if (!unitVal || unitVal.length === 1 && unitVal[0] === "*")
+        this.patternObject[unit] = void 0
+    })
+  }
+  private purgeRanges() {
+    this.iteratePlaces((place, unit) => {
+      place.map(placeElement => {
+        if (!this.isRange(placeElement)) return
 
-    private checkPlaceSemantics(place: Array<any>, unit): void {
-        place.forEach(element => {
-            if (typeof(element) === 'number' 
-            && !DateUtil.isValidTimeUnitValue(element, unit)) 
-                throw new Error(`Invalid value ${element} for unit ${unit}`)
-            
-            if(typeof(element) !== 'number' 
-            && !this.checkArrayValues(element.slice(1), unit))
-                throw new Error(`Invalid values ${element} for unit ${unit}`) });}
-    /**
-     * Now used for both interval and range
-     */
-    private checkArrayValues(array: Array<number>, unit: string): boolean {
-        if (array[0] > array[1])
-            throw new Error(
-                `Both interval and range syntax require left limit to be smaller.`);
-        for (let x of array) {
-            if (!DateUtil.isValidTimeUnitValue(x, unit)) return false
-        }
-        return true
+      })
+    })
+  }
+
+  private isRange(a: PlaceElement): boolean {
+    return a[0] === 'range'
+  }
+  private spreadRange(r: Range): Array<number> {
+    const retVal = []
+    for (let i = r[1]; i <= r[2]; i++)
+      retVal.push(i)
+    return retVal
+  }
+
+  private iteratePlaces(cb: (place: Array<PlaceElement>, unit: TimeUnit) => void) {
+    for (let unit of TimeUnit) {
+      let value = this.patternObject[unit];
+      if (!value) continue
+      cb(value, <TimeUnit>unit);
     }
-/**/
+  }
+  /**
+   * Check if patternObject satisfies TimeUnitValue 
+   */
+  private checkPatternSemantics(): void {
+    this.iteratePlaces(this.checkPlaceSemantics)
+  }
+
+  private checkPlaceSemantics(place: Array<any>, unit): void {
+    place.forEach(element => {
+      if (typeof (element) === 'number'
+        && !DateUtil.isValidTimeUnitValue(element, unit))
+        throw new Error(`Invalid value ${element} for unit ${unit}`)
+
+      if (typeof (element) !== 'number'
+        && !this.checkArrayValues(element.slice(1), unit))
+        throw new Error(`Invalid values ${element} for unit ${unit}`)
+    });
+  }
+  /**
+   * Now used for both interval and range
+   */
+  private checkArrayValues(array: Array<number>, unit: string): boolean {
+    if (array[0] > array[1])
+      throw new Error(
+        `Both interval and range syntax require left limit to be smaller.`);
+    for (let x of array) {
+      if (!DateUtil.isValidTimeUnitValue(x, unit)) return false
+    }
+    return true
+  }
+  /**/
 }
 
 // ✅❓ ------------------------------------------------------------------------
 if (process.argv.length > 2 && process.argv[2] === 'sa-test')
-    runSaTest()
+  runSaTest()
 
 function runSaTest() {
-    let test = new DiminishedCron('0 3~44,5-7,8 3-5,6 * * 5');
-    console.log(test.patternObject);
-    
+  let test = new DiminishedCron('0 3~44,5-7,8 3-5,6 * * 5');
+  console.log(test.patternObject);
+
 }
